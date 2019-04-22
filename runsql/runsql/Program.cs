ef5e910609f71f4b70e0acd263281975ea4ea505
unknown <rechvladimir@hotmail.com>
2019-04-22 10:27:09 -0300

    Fixing start parameters bug.

--- runsql/runsql/Program.cs
+++ runsql/runsql/Program.cs
@@ -1,35 +1,108 @@
 using System;
+using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace runsql
 {
     class Program
     {
+        static string[] _help = new string[] { "/?", "-h", "-help" };
+        static string[] _validOperations = new string[] { "-exec", "-query" };
         static void Main(string[] args)
         {
-            //1-arquivo
-            //2-servidor
-            //3-base de dados
-            //4-user id
-            //5-password
-            if (args.Length != 5)
+            /* Uso 
+             * 
+             * runsql -exec file.sql server database userid password
+             * 
+             * runsql -query file.sql server database userid password delimiter
+             * 
+             * 
+             */
+            if (args.Length == 0 || _help.Contains(args[0]) || !_validOperations.Contains(args[0]) || (args[0] == "-query" &&  args.Length != 7) ||
+                (args[0] == "-query" && args.Length < 6))
             {
-                Console.WriteLine("Usage runsql sqlfile server database user_id password");
+                Console.WriteLine(@"Uso 
+
+    runsql -exec file.sql server database userid password
+
+    runsql -query file.sql server database userid password delimiter");
                 return;
             }
 
-            string fileName = args[0];
-            string serverName = args[1];
-            string database = args[2];
-            string user = args[3];
-            string pass = args[4];
+            string fileName = args[1];
+            string serverName = args[2];
+            string database = args[3];
+            string user = args[4];
+            string pass = args[5];
+            string separator = args.Length == 7 ? args[6] : "";
             string connString = String.Format("Data source={0}; Initial catalog={1};" +
                 "User id={2}; password={3}", serverName, database, user, pass);
             int counter = 1;
 
+            if (args[0].ToLower() == "-exec")
+                exec(fileName, connString, counter);
+            else
+                query(fileName, connString, counter, separator);
+
+            Console.WriteLine("Concluído");
+        }
+
+        private static void query(string fileName, string connString, int counter, string separator)
+        {
+            Console.Title = "Preparando conexão";
+            using (var con = new SqlConnection(connString))
+            {
+                Console.Title = "Abrindo conexão";
+                con.Open();
+
+                Console.Title = "Abrindo arquivo";
+
+                using (var f = System.IO.File.OpenText(fileName))
+                {
+                    while (f.Peek() > -1)
+                    {
+                        Console.Title = "Lendo linha " + counter.ToString();
+                        using (SqlDataAdapter da = new SqlDataAdapter(f.ReadLine(), con))
+                        {
+                            try
+                            {
+                                DataTable dt = new DataTable();
+                                da.Fill(dt);
+
+                                for (int i = 0; i < dt.Rows.Count; i++)
+                                {
+                                    for (int j = 0; j < dt.Columns.Count; j++)
+                                    {
+                                        Console.Write("{0}{1}", dt.Rows[i][j], separator);
+                                    }
+
+                                    Console.Write(System.Environment.NewLine);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+
+                            counter++;
+                        }
+                    }
+                }
+
+                con.Close();
+            }
+        }
+
+        private static void exec(string fileName, string connString, int counter)
+        {
+            Console.Title = "Abrindo conexão.";
+
             using (var con = new SqlConnection(connString))
             {
                 con.Open();
+
+                Console.Title = "Abrindo arquivo de texto.";
 
                 using (var f = System.IO.File.OpenText(fileName))
                 {
@@ -37,13 +110,15 @@ namespace runsql
                     {
                         string errorMessage = null;
 
+                        Console.Title = string.Format("Executando linha: {0:000000}", counter);
+
                         using (SqlCommand cmd = new SqlCommand(f.ReadLine(), con))
                         {
                             try
                             {
                                 cmd.ExecuteNonQuery();
                             }
-                            catch(Exception ex)
+                            catch (Exception ex)
                             {
                                 errorMessage = ex.Message;
                             }
@@ -57,6 +132,5 @@ namespace runsql
                 con.Close();
             }
         }
-
     }
 }
