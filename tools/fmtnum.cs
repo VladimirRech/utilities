1b19a6d0a870d67fe4f750d17edad7663fb103e4
unknown <rechvladimir@hotmail.com>
2019-05-08 17:47:42 -0300

    New utility.

---
+++ tools/fmtnum.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+public class program {
+	const string _error = "Usage: fmtnum number format";
+	const string _help = "\n\tNumber: numeric chars to format\n\tFormat: ex: ###.###.###-##";
+	static string[] _help_sign = new string[] { "--help", "-h", "--h", "-?", "/?", "/h", "/help" };
+
+	public static void Main(string[] args){
+		if (args.Length != 2) {
+			Console.WriteLine(_error);
+			return;
+		}
+		else if (_help_sign.Contains(args[0])){
+			Console.WriteLine(_error);
+			Console.WriteLine(_help);
+			return;
+		}
+	}
+}
